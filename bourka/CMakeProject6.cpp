01dec131418f373c38c20e0700caae74f2ead235
Username4200 <tomasort1@email.cz>
2025-10-06 19:06:59 +0200

    Cvičení 3. Bouřka + Funex

---
+++ bourka/CMakeProject6.cpp
@@ -0,0 +1,30 @@
+﻿// CMakeProject6.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject6.h"
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << "Zadejte pocet sekund od hromu: "; // výzva k zadání čísla
+	cin >> a; // zadání čísla 
+
+	double s = a * 340; // výpočet vzdálenosti v metrech
+
+
+
+
+
+
+	cout << "Blesk uhodil  " << s << " metru od vas" << endl; // výpis čísla
+
+
+
+
+
+
+	return 0;
+}
