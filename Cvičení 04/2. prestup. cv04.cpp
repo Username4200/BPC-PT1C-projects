f77848cd5d88a3859b1903a64af45fbe5bb78089
Username4200 <tomasort1@email.cz>
2025-10-09 10:06:59 +0200

    Cviceni 04

---
+++ Cvičení 04/2. prestup. cv04.cpp
@@ -0,0 +1,59 @@
+﻿// 2. prestup. cv04.cpp : Defines the entry point for the application.
+//
+
+#include "2. prestup. cv04.h"
+
+using namespace std;
+
+int jeprestupny(int rok)
+{
+	if (rok % 4 == 0)
+	{
+		if (rok % 100 != 0)
+			return (1);
+	}
+
+	if (rok % 400 == 0)
+		return 1;
+
+	else
+		return 0;
+
+	
+
+
+
+}
+
+
+
+ 
+
+
+int main()
+{
+	
+	int start, end;
+
+	printf("Zadejte prvni rok:  ");
+	scanf("%d", &start);
+
+	printf("Zadejte poslední rok:   ");
+	scanf("%d", &end);
+
+	for (int i = start; i <= end; i++) {
+		printf("Rok %d je prestupny: %d \n", i, jeprestupny(i));
+
+
+
+
+
+
+	}
+
+
+
+
+
+	return 0;
+}
---
+++ Cvičení 04/CMakeProject1.cpp
@@ -0,0 +1,49 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+
+int jeprestupny(int rok)
+{
+	if (rok % 4 == 0 )
+		
+
+	{
+		if (rok % 100 != 0)
+			return 1;
+		
+	}
+
+	if (rok % 400 == 0)
+		return 1;
+	else
+		return 0;
+
+
+
+
+}
+
+int main()
+{
+	printf(" rok 1000 %d\n", jeprestupny(1000));
+
+	printf(" rok 2000 %d\n", jeprestupny(2000));
+
+	printf(" rok 2002 %d\n", jeprestupny(2002));
+
+	printf(" rok 2012 %d\n", jeprestupny(2012));
+
+	printf(" rok 2022 %d\n", jeprestupny(2022));
+
+	printf(" rok 2200 %d\n", jeprestupny(2200));
+
+	printf(" rok 2024 %d\n", jeprestupny(2024));
+
+	
+
+	return 0;
+}
