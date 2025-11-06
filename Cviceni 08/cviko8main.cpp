3aab92aa50013baf70ceb3b43eccee270ec26a9e
Username4200 <tomasort1@email.cz>
2025-11-06 09:57:11 +0100

    Cvicení 6.11

---
+++ Cviceni 08/cviko8main.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include "bartonek.h"
+
+int main() {
+    double a1, b1, a2, b2;
+    double pr_zac, pr_kon;
+
+    printf("Zadejte 1. interval:\n");
+    scanf("%lf %lf", &a1, &b1);
+
+    printf("Zadejte 2. interval:\n");
+    scanf("%lf %lf", &a2, &b2);
+
+    if (prunik(a1, b1, a2, b2, &pr_zac, &pr_kon)) {
+        printf("Prunik intervalu (%.1f, %.1f) a (%.1f, %.1f) je (%.1f %.1f)\n",
+            a1, b1, a2, b2, pr_zac, pr_kon);
+    }
+    else {
+        printf("Prunik intervalu (%.1f, %.1f) a (%.1f, %.1f) neexistuje\n",
+            a1, b1, a2, b2);
+    }
+
+    return 0;
+}
